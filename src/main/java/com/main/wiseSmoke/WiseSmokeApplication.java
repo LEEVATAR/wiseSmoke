a6bfe69ebf207f9748815328e59177db4856930b
LEEVATAR <thega4004@naver.com>
2023-07-19 03:02:27 +0900

    0719 first commit test on iJ

---
+++ src/main/java/com/main/wiseSmoke/WiseSmokeApplication.java
@@ -0,0 +1,13 @@
+package com.main.wiseSmoke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiseSmokeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiseSmokeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/main/wiseSmoke/WiseSmokeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.wiseSmoke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiseSmokeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
